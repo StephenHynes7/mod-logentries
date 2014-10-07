6b9f73b86e54441509d5640ff48cb48d40448860
Stephen Hynes <stephen.hynes@logentries.com>
2014-10-07 14:17:03 +0100

    update comments, install instructions and package tags

--- module/module.py
+++ module/module.py
@@ -3,10 +3,7 @@
 # -*- coding: utf-8 -*-
 
 # Copyright (C) 2009-2012:
-#    Gabes Jean, naparuba@gmail.com
-#    Gerhard Lausser, Gerhard.Lausser@consol.de
-#    Gregory Starck, g.starck@gmail.com
-#    Hartmut Goebel, h.goebel@goebel-consult.de
+#    Hynes Stephen, sthynes8@gmail.com
 #
 # This file is part of Shinken.
 #
@@ -23,9 +20,8 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with Shinken.  If not, see <http://www.gnu.org/licenses/>.
 
-"""This Class is a plugin for the Shinken Broker. It is in charge
-to brok information of the service/host perfdatas into the Graphite
-backend. http://graphite.wikidot.com/start
+"""This Class is a plugin for Logentries which will forward data to your Logentris account.
+   A free 30 Day Trial can be started here https://logentries.com/quick-start/
 """
 
 import urllib2
@@ -40,23 +36,25 @@ from shinken.log import logger
 
 properties = {
     'daemons': ['broker'],
-    'type': 'logentries_perfdata',
+    'type': 'log_data',
     'external': False,
 }
 
 
 def get_instance(plugin):
-    logger.debug("Get a Simple log broker for plugin %s" % plugin.get_name())
+    logger.debug("Get a Logentries broker for plugin %s" % plugin.get_name())
     instance = Logentries_Broker(plugin)
     return instance
 
 
 class Logentries_Broker(BaseModule):
+    # Class for Logentries Broker
 
     def __init__(self, modconf):
         BaseModule.__init__(self, modconf)
         self.uuid_regex = '[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}'
-        self.host = getattr(modconf, 'host', 'https://js.logentries.com/v1/logs/')
+        self.host = getattr(
+            modconf, 'host', 'https://js.logentries.com/v1/logs/')
         self.token = getattr(modconf, 'token', None)
         if self.token is None:
             self.is_uuid = bool(re.match(self.uuid_regex, self.token))
@@ -67,21 +65,23 @@ class Logentries_Broker(BaseModule):
         self.queue = deque([])
 
     def init(self):
-        logger.info("[Graphite broker] I init the %s server connection to %s" %
-                    (self.get_name(), str(self.endpoint)))
+        logger.info(
+            "[Logentries Broker] I init the %s server connection to %s" %
+            (self.get_name(), str(self.endpoint)))
 
     def send_data(self):
         while len(self.queue) > 0:
             data = self.queue.popleft()
-            timestamp = datetime.datetime.fromtimestamp(time.getime()).strftime('%H:%M:%S %d-%m-%Y')
-            msg = json.dumps({"event": {'timestamp': timestamp, 'data': data['log']}})
+            timestamp = datetime.datetime.fromtimestamp(
+                time.getime()).strftime('%H:%M:%S %d-%m-%Y')
+            msg = json.dumps(
+                {"event": {'timestamp': timestamp, 'data': data['log']}})
             req = urllib2.Request(self.endpoint, msg)
             try:
                 urllib2.urlopen(req)
             except urllib2.URLError as e:
                 logger.error("Can't send log message to Logentries %s", e)
 
-    # A service check have just arrived, we UPDATE data info with this
     def manage_logentries_brok(self, b):
         data = b.data
         if data is None:
@@ -90,4 +90,3 @@ class Logentries_Broker(BaseModule):
         if len(self.queue) >= self.queue_size:
             logger.debug("Queue is full, sending logs to Logentries")
             self.send_data()
-
